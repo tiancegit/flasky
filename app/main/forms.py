5df292992095cd61be7ad86ef83d46e73b2d8073
lee <tiance.1984@gmail.com>
2016-12-07 15:19:11 +0800

    基础完成

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,9 @@
 #!coding:utf-8
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 
-class NameForm(Form):    # 定义表单
+class NameForm(FlaskForm):    # 定义表单
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
