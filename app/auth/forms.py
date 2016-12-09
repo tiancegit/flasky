0f5cb7ee1588ff5e8f27e03633f5611b1dc70a08
lee <tiance.1984@gmail.com>
2016-12-09 21:17:49 +0800

    add 用户登录

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,15 @@
 #!coding:utf-8
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, length, Email
+from wtforms.validators import Required, length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+
 '''
 电子邮件字段用到了 WTForms 提供的 Length() 和 Email() 验证函数。 PasswordField 类表
 示属性为 type="password" 的 <input> 元素。 BooleanField 类表示复选框。
-
 '''
 
 class LoginForm(FlaskForm):
@@ -13,3 +17,42 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me Logged in')
     submit = SubmitField('Log In')
+
+
+'''
+注册页面使用的表单要求用户输入电子邮件地址，用户名，密码，这个表单如下所示。
+'''
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Email", validators=[Required(), length(1, 64), Email()])
+    username = StringField('Username', validators=[Required(), length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                     'Usernames must have only letters,'
+                                                                                     'numbers,dots or Underscores')])
+    # 这里使用了WTForms提供的Regexp验证函数，确保username字段只包含了字母数字下划线和点号，这个验证函数中正则表达式后面的两个参数分别是
+    # 正则表达式的旗标和验证失败是显示的错误信息。
+
+    # 安全起见，密码要输入两次，此时要验证两个密码字段的值是否一致，这种验证可使用WTForms的一个验证函数实现，即 Equalto 。
+    # 这个验证函数要附属到两个密码字段中的一个上，另一个要作为参数传入。
+
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Password must match')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    # 这个表单中还有两个自定义的验证函数，以方法的形式实现，如果表单类中定义了validate_开头，且跟着字段名的方法，这个方法就和
+    # 常规的验证函数一起调用，这里分别为email和username字段定义了验证函数，确保填写的值没有在数据库中出现过，自定义的验证函数
+    # 想要表示验证失败，可以抛出ValidationError异常，其参数就是错误信息。
+
+    # 现在表单层面处理好邮件和用户名重复的问题，数据没问题了再交给视图函数处理。
+
+
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in user.')
+
+    # 显示这个表单的模板是/templates/auth/register.html，和登录模板一样，这个模板也使用wtf.quick_form()渲染表单。
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,23 @@
 #!coding:utf-8
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from flask_login import login_required, login_user
-from ..models import User
-from .forms import LoginForm
+from flask_login import login_required, login_user, logout_user
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
+
+
+''' 这个函数创建了一个LoginForm对象，当请求类型是Get的时候，视图直接渲染模板，既显示表单，当表单在POST请求中提交时，Flask-wtf中的
+validate_on_submit()函数会验证表单数据，然后尝试登入用户。
+
+为了登入用户，视图函数首先使用表单中填写的email从数据库中加载用户。如果电子邮件的对应用户存在，再调用用户对象的verify_password()
+方法，其参数是表单中填写的密码，如果密码正确，则调用Flask-login中的Login_user()函数，在用户会话中把用户标记为已登录
+login_user()函数的参数是要登录的用户，以及可选的 ‘记住我’ 布尔值，‘记住我’也要在表单中填写，如果值为False,那么关闭浏览器后，
+用户会话则过期，所以下次用户访问时要重新登录，如果值为True，那么会在用户浏览器中写入一个长期有效的Cookie，使用这个cookie可以复现用户会话。
+
+提交密令的POST请求最后也做了重定向，不过目标URL有两种可能，用户访问未授权的URL是会显示登录表单，Flask-Login会把原地址保存在查询字符串的Next
+参数，这个参数可以从request.args字典中读取。如果查询字符串中没有next参数，则重定向发到首页，如果用户输入的电子邮件或者密码不正确，程序会设定一个Flash消息，
+再次渲染表单，让用户重试登录
+'''
 
 
 @auth.route('/login', methods=["GET", "POST"])
@@ -19,7 +33,34 @@ def login():
 
 
 
+'''~~ 在生产服务器上，登录路由必须使用安全的HTTP。从而加密传送给服务器的表单数据，如果没有使用安全的Http，登录密令在传输的过程中可能会被截取，在服务器上花再多的
+的精力来保证密码安全都是无济于事。'''
 
+'''为了登出用户，这个视图函数调用Flask_login中的logout_user()函数，删除并重设了会话，随后会显示一个flash信息，确认这次操作，
+再重定向到首页，这样登出就完成了。'''
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+'''提交注册表单后，通过验证后，系统就使用用户填写的信息在数据库中注册一个新用户，处理这个任务的视图函数如下'''
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash("You can now login")
+        return redirect(url_for('auth.register'))
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/secret')
