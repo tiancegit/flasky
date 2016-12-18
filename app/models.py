3d0a620f7bdc113f000eaa8880030fc20c47f990
lee <tiance.1984@gmail.com>
2016-12-18 23:06:30 +0800

    add 修改邮箱地址  视图函数部分

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 #!coding:utf-8
 from flask import current_app
 from flask_login import UserMixin
-from itsdangerous import TimedJSONWebSignatureSerializer as Serizlizer    # 使用itsdangerous生成令牌
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer    # 使用itsdangerous生成令牌
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
@@ -74,7 +74,7 @@ class User(UserMixin, db.Model):
     '''
 
     def generate_confirmation_token(self, expiration=3600):
-        s = Serizlizer(current_app.config['SECRET_KEY'], expiration)
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id})
 
     '''
@@ -83,7 +83,7 @@ class User(UserMixin, db.Model):
     '''
 
     def confirm(self, token):
-        s = Serizlizer(current_app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
         except:
@@ -95,11 +95,11 @@ class User(UserMixin, db.Model):
         return True
 
     def generate_reset_token(self, expiration=3600):
-        s = Serizlizer(current_app.config['SECRET_KEY'], expiration)
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset': self.id})
 
     def reset_password(self, token, new_password):
-        s = Serizlizer(current_app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
         except:
@@ -113,18 +113,18 @@ class User(UserMixin, db.Model):
     # expiration 令牌的过期时间
 
     def generate_email_change_token(self, new_email, expiration=3600):
-        s = Serizlizer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'change_email': self.id, new_email: new_email})
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
 
     def change_email(self, token):
-        s = Serizlizer(current_app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
         except:
             return False
         if data.get('change_email') != self.id:
             return False
-        new_email = data.get('chang_email')
+        new_email = data.get('new_email')
         if new_email is None:
             return False
         if self.query.filter_by(email=new_email).first() is not None:
@@ -137,6 +137,8 @@ class User(UserMixin, db.Model):
         return '<User %r>' % self.username
 
 # 这段代码作用未明确
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
